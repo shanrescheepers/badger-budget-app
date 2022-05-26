7d4cda4f3384e1c5fd79e62629c75029411d2552
Shanre Scheepers <21100387@virtualwindow.co.za>
2022-05-26 12:43:18 +0200

    FIX

--- src/components/rightpanel.js
+++ src/components/rightpanel.js
@@ -26,6 +26,48 @@ export function RightPanel() {
                     <input placeholder="$" className="total-saved-input">
                     </input></InputGroup>
             </div>
+            <br>
+            </br>
+            <div className="household-block">
+                <h3 className="save-area-font">Household Summary</h3>
+                <div class="form-row" >
+                    <div class="form-group col-md-6">
+                        <InputGroup className="household">
+                            <input placeholder="Total Income" className="savers-name">
+                            </input></InputGroup>
+                        <div className="saving-amount">
+                            <InputGroup className="savers-name">
+                                <input placeholder="$" className="savers-name">
+                                </input></InputGroup>
+                        </div>
+                    </div>
+                    <div class="form-group col-md-6">
+                        <InputGroup className="tot-expense">
+                            <input placeholder="Total Expenses" className="savers-name">
+                            </input></InputGroup>
+                        <div className="saving-amount">
+                            <InputGroup className="savers-name">
+                                <input placeholder="$" className="savers-name">
+                                </input></InputGroup>
+                        </div>
+                    </div>
+
+                    <div class="form-group col-md-6">
+                        <InputGroup className="tax">
+                            <input placeholder="Tax" className="savers-name">
+                            </input></InputGroup>
+                        <div className="saving-amount">
+                            <InputGroup className="savers-name">
+                                <input placeholder="$" className="savers-name">
+                                </input></InputGroup>
+                        </div>
+                    </div>
+                </div>
+                <hr className="household-whatsleft"></hr>
+                <p className="total-saved">What's left : </p>  <InputGroup className="total-saved-group">
+                    <input placeholder="$0" className="total-saved-input">
+                    </input></InputGroup>
+            </div>
         </div>
 
     )
