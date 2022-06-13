97f5d875ddd68ea5e1ac2626cb3550da08ccc20a
Shanre Scheepers <21100387@virtualwindow.co.za>
2022-06-13 16:04:57 +0200

    CLEANUP

--- src/App.js
+++ src/App.js
@@ -1,22 +1,8 @@
-
 import './App.css';
-import SideNav from './components/sidenav';
-import { Route, Routes } from 'react-router-dom';
-import Dashboard from './components/dashboard';
-// import RightPanel from './components/rightpanel';
 
 function App() {
   return (
     <div className="App">
-      {/* <SideNav /> */}
-
-      <Routes>
-
-
-        <Route path="/" element={<Dashboard />} />
-
-      </Routes>
-
     </div>
   );
 }
