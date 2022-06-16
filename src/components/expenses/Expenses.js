85e97c4ed16b4c6d88c5de06acd18afac42cd5e9
Shanre Scheepers <21100387@virtualwindow.co.za>
2022-06-16 18:17:59 +0200

    Basic Grid Design : Expenses

--- src/components/expenses/Expenses.js
+++ src/components/expenses/Expenses.js
@@ -1,12 +1,28 @@
-import { useEffect } from "react";
+import './Expenses.scss';
 
 
 function Expenses(expensesData) {
 
     return (
-        <section>
-            <h1>Expenses Data: {JSON.stringify(expensesData)}</h1>
-        </section>
+        <div className="expenses">
+            <h1> Expenses</h1>
+            <form className="expenses__add">
+                <input placeholder="expense name"></input>
+                <input placeholder="expense name"></input>
+                <button>ADD</button>
+            </form>
+            <h2 className='expenses__heading'>Added Expenses</h2>
+            <div className='expenses__content'>
+                <div className='expenses__content__row'>
+                    <span>Expense Name</span>
+                    <span>R X</span>
+                </div>
+            </div>
+            <div className='expenses__remove-btn'>
+                <button>REMOVE</button>
+            </div>
+            <h2 className='expenses__total'>TOTAL EXPENSES: R X</h2>
+        </div >
     );
 }
 
