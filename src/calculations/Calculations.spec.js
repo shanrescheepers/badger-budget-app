3a1a80b9005bf2152df357fdcdb91bc54e13cc30
Shanre Scheepers <21100387@virtualwindow.co.za>
2022-06-25 02:45:59 +0200

    Testing files attempt 1/baie

--- src/calculations/Calculations.spec.js
+++ src/calculations/Calculations.spec.js
@@ -1,4 +1,4 @@
-import { calculateNet, calculateSaveAmount, calculateTaxBracket } from "../../calculations/Calculations";
+import { calculateExpenseLumpTotal, calculateNet, calculateSaveAmount, calculateTaxBracket } from "./Calculations";
 
 describe("Income tax bracket category positive tests", function () {
     var taxBracket;
@@ -115,4 +115,36 @@ describe("Test after savings calculations", function () {
 
         expect(calculatedSaveAmount).not.toBe(11000);
     })
+})
+
+describe("Test total expense calculation (LUMP)", function () {
+    var sampleExpenses;
+    var expectedTotal;
+
+    beforeEach(function () {
+        sampleExpenses = [
+            {
+                expenseTotal: 12
+            },
+            {
+                expenseTotal: 59
+            },
+            {
+                expenseTotal: 29
+            }
+        ];
+        expectedTotal = 100;
+    })
+
+    it("Expense calculation positive test", function () {
+        var calculatedTotal = calculateExpenseLumpTotal(sampleExpenses);
+
+        expect(calculatedTotal).toBe(expectedTotal);
+    });
+
+    it("Expense calculation negative test", function () {
+        var calculatedTotal = calculateExpenseLumpTotal(sampleExpenses);
+
+        expect(calculatedTotal).not.toBe(expectedTotal + 10);
+    });
 })
